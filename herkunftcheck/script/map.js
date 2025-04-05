ca19705226bff7c0d9f7c4b092885518a897c316
Sascha Kober <100568350+KoberSascha@users.noreply.github.com>
2025-04-05 21:13:25 +0200

    [ADD] - layout

--- herkunftcheck/script/map.js
+++ herkunftcheck/script/map.js
@@ -3,7 +3,7 @@ function InitializeMapAndData() {
 
     // Initialize Map
 
-    var map = L.map('map', { zoomControl: true });
+    var map = L.map('map', { zoomControl: false });
  
     /*L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
